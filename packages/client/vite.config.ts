8ea910660c983edd2a63e94daa94b3e4cb3ae1d5
Ansgar <ansgar.doersam@online.de>
2026-05-14 02:10:21 +0200

    Update extensions and settings, add some path aliases

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -7,10 +7,15 @@ import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), ui()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    plugins: [vue(), vueDevTools(), ui()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@view': fileURLToPath(new URL('./src/views', import.meta.url)),
+            '@store': fileURLToPath(new URL('./src/stores', import.meta.url)),
+            '@component': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@asset': fileURLToPath(new URL('./src/assets', import.meta.url)),
+            '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+        },
     },
-  },
 })
